f37634e4189110e9bea442cdd4053b516b73310a
Alà Eddin Abugrara <al@maryam.fritz.box>
2019-07-24 18:26:17 +0200

    add Event.js for testing

---
+++ src/Event.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Event extends Component {
+  state = {
+    events: [],
+  }
+
+  render() {
+    return (
+      // this.state.events
+      events
+    );
+  }
+}
+
+export default Event;
--- src/EventList.js
+++ src/EventList.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Event from './Event';
 
 class EventList extends Component {
   render() {
     return (
-      <div></div>
+      <ul className="EventList">
+        {this.state.events.map(event =>
+          <li key={event.id}>
+            <Event event={event} />
+          </li>
+        )}
+      </ul>     
     );
   }
 }
