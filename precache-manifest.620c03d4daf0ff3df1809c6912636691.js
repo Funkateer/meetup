82021b2220739e304b211ab3a43b260528e87c78
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-08-24 20:35:38 +0200

    Updates

---
+++ precache-manifest.620c03d4daf0ff3df1809c6912636691.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4585cb7632eb99ac404eecc6c9192293",
+    "url": "/meetup/index.html"
+  },
+  {
+    "revision": "67271cf66c16aca0cbf3",
+    "url": "/meetup/static/css/main.fdec4a4f.chunk.css"
+  },
+  {
+    "revision": "3d8e004bf673e59ff6d2",
+    "url": "/meetup/static/js/2.db0827aa.chunk.js"
+  },
+  {
+    "revision": "67271cf66c16aca0cbf3",
+    "url": "/meetup/static/js/main.2d1e4542.chunk.js"
+  },
+  {
+    "revision": "c81de03e5fcc2eb84eed",
+    "url": "/meetup/static/js/runtime~main.25e5dfeb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.4efe9c12e50081c7b4629c99b4ed99c0.js"
+  "/meetup/precache-manifest.620c03d4daf0ff3df1809c6912636691.js"
 );
 
 self.addEventListener('message', (event) => {
